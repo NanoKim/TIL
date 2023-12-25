4a3d5faa83e32d0dd36603551da44252f79d0b46
Jaeyeong Kim <kjyyy7341@gmail.com>
2023-12-25 10:31:37 +0900

    내용정리

---
+++ oracle/내용정리/NANO_00_모음_KH계정.sql
@@ -0,0 +1,18 @@
+/*
+
+    < 날짜 처리 >
+    
+    SYSDATE : 시스템의 현재 날짜 및 시간
+    MONTHS_BETWEEN(DATE1, DATE2) : DATE 사이의 개월 수
+    ADD_MONTHS(DATE, NUMBER) : DATE 에 NUMBER 개월 더하기
+    NEXT_DAY(DATE, 요일(문자|숫자)) : DATE 이후의 가까운 요일 날짜 반환
+    LAST_DAY(DATE) : 해당 월의 마지막 날짜 반환
+    EXTRACT(YEAR|MONTH|DAY FROM DATE) : 특정 날짜로부터 년도|월|일 값 리턴
+    
+
+
+
+
+
+
+*/
